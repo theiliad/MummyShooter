3c632dd9e12205a55291710d66f60c090d1e551b
The Iliad <iliadmoosavi@gmail.com>
2018-02-09 23:49:03 -0500

    Swaying initial version

---
+++ Assets/Scripts/Weapon/WeaponSway.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSway : MonoBehaviour {
+	public float sensitivity;
+	public float speed;
+
+	private Vector3 initialPosition;
+
+	void Start () {
+		initialPosition = transform.localPosition;
+	}
+	
+	void Update () {
+		float moveX = -Input.GetAxis("Mouse X") * sensitivity;
+		float moveY = -Input.GetAxis("Mouse Y") * sensitivity;
+
+		Vector3 finalPosition = new Vector3(moveX, moveY, 0);
+		transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition + initialPosition, Time.deltaTime * speed);
+	}
+}
