483ef252a00f75237479936be775f0a20c9b14ba
The Iliad <iliadmoosavi@gmail.com>
2018-02-09 23:58:44 -0500

    Sway is clamped now

--- Assets/Scripts/Weapon/WeaponSway.cs
+++ Assets/Scripts/Weapon/WeaponSway.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponSway : MonoBehaviour {
-	public float sensitivity;
-	public float speed;
+	public float sensitivity = 0.2f;
+	public float maxAmount = 0.08f;
+	public float speed = 6;
 
 	private Vector3 initialPosition;
 
@@ -15,6 +16,8 @@ public class WeaponSway : MonoBehaviour {
 	void Update () {
 		float moveX = -Input.GetAxis("Mouse X") * sensitivity;
 		float moveY = -Input.GetAxis("Mouse Y") * sensitivity;
+		moveX = Mathf.Clamp(moveX, -maxAmount, maxAmount);
+		moveY = Mathf.Clamp(moveY, -maxAmount, maxAmount);
 
 		Vector3 finalPosition = new Vector3(moveX, moveY, 0);
 		transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition + initialPosition, Time.deltaTime * speed);
